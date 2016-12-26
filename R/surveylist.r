2f5ca72d39da407e96bdb9269efb057edd46e8f5
Sean Fahey <seanofahey@gmail.com>
2016-12-26 09:19:19 -0500

    removed api_key references, updated api url to v3, changed post to get, commented out content$status lines

--- R/surveylist.r
+++ R/surveylist.r
@@ -7,17 +7,13 @@ surveylist <- function(
     recipient_email = NULL,
     order_asc = NULL,
     fields = NULL,
-    api_key = getOption('sm_api_key'),
     oauth_token = getOption('sm_oauth_token'),
     ...
 ){
-    if(!is.null(api_key)) {
-        u <- paste('https://api.surveymonkey.net/v2/surveys/get_survey_list?',
-                    'api_key=', api_key, sep='')
-    } else
-        stop("Must specify 'api_key'")
-    if(!is.null(oauth_token))
+    if(!is.null(oauth_token)){
+        u <- 'https://api.surveymonkey.net/v3/surveys?'
         token <- paste('bearer', oauth_token)
+    }
     else
         stop("Must specify 'oauth_token'")
     if(inherits(start_date, "POSIXct") | inherits(start_date, "Date"))
@@ -35,40 +31,12 @@ surveylist <- function(
         b <- toJSON(b[!nulls], auto_unbox = TRUE)
     h <- add_headers(Authorization=token,
                      'Content-Type'='application/json')
-    out <- POST(u, config = h, ..., body = b)
+    out <- GET(u, config = h, ..., body = b)
     stop_for_status(out)
-    content <- content(out, as='parsed')
-    if(content$status != 0){
-        warning("An error occurred: ",content$errmsg)
-        return(content)
-    } else 
-        lapply(content$data$surveys, `class<-`, 'sm_survey')
-}
-
-print.sm_survey <- function(x, ...){
-    if(!is.null(x$title)) {
-        if(is.list(x$title))
-            cat('Survey Title:', x$title$text, '\n')
-        else
-            cat('Survey Title:', x$title, '\n')
-    }
-    if(!is.null(x$survey_id))
-        cat('ID:', x$survey_id, '\n')
-    if(!is.null(x$language_id))
-        cat('Language:', x$language_id, '\n')
-    if(!is.null(x$question_count))
-        cat('No. of Questions:', x$question_count, '\n')
-    if(!is.null(x$num_responses))
-        cat('Respondents:', x$num_responses, '\n')
-    if(!is.null(x$preview_url))
-        cat('Preview URL:', x$preview_url, '\n')
-    if(!is.null(x$analysis_url))
-        cat('Analysis URL:', x$analysis_url, '\n')
-    if(!is.null(x$date_created))
-        cat('Date Created: ', x$date_created, '\n')
-    if(!is.null(x$date_modified))
-        cat('Date Modified:', x$date_modified, '\n')
-    if(!is.null(x$pages))
-        cat('Survey Pages:', length(x$pages), '\n')
-    invisible(x)    
+    content <- parsed_content(out)
+    # if(content$status != 0){
+    #     warning("An error occurred: ",content$errmsg)
+    #     return(content)
+    # } else 
+    # lapply(content$data$surveys, `class<-`, 'sm_survey')
 }
