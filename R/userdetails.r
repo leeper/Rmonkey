dd1f4a37618a000e82f63030c2cb7e730a3fc0f6
Sean Fahey <seanofahey@gmail.com>
2016-12-27 14:00:50 -0500

    changed POST to GET, updated class function, commented out content$status warning

--- R/userdetails.r
+++ R/userdetails.r
@@ -6,11 +6,11 @@ userdetails <- function(
     if(!is.null(oauth_token))
         token <- paste('bearer', oauth_token)
     else
-        stop("Must specify 'oauth_token'")
-    out <- POST(u, config = add_headers(Authorization=token))
+        stop("Must specify 'oauth_token'.  Try smlogin() first to get a token.")
+    out <- GET(u, config = add_headers(Authorization=token))
     stop_for_status(out)
     content <- parsed_content(out)
-    if(content$status != 0)
-        warning("An error occurred: ",content$errmsg)
-    structure(content$data$user_details, class='sm_userdetails')
+    # if(content$status != 0)
+    #     warning("An error occurred: ",content$errmsg)
+    structure(content$data$user_details, class="sm_userdetails")
 }
