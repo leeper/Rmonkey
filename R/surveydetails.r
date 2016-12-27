fc8d0e94bcc0b28b8146ce33ef870e86e26970ea
Sean Fahey <seanofahey@gmail.com>
2016-12-26 19:10:15 -0500

    updated to use SurveyMonkey API V3 urls, removed api_key references, removed body JSON now in url, modified class structure line, added new function call to get question_details

--- R/surveydetails.r
+++ R/surveydetails.r
@@ -1,42 +1,41 @@
 surveydetails <- function(
     survey,
-    api_key = getOption('sm_api_key'),
     oauth_token = getOption('sm_oauth_token'),
+    question_details = FALSE,
     ...
 ){
     if(inherits(survey, 'sm_survey'))
-        survey <- survey$survey_id
-    if(!is.null(api_key)) {
-        u <- paste('https://api.surveymonkey.net/v2/surveys/get_survey_details?',
-                    'api_key=', api_key, sep='')
-    } else
-        stop("Must specify 'api_key'")
-    if(!is.null(oauth_token))
-        token <- paste('bearer', oauth_token)
+        survey <- survey$id
+    if(question_details) {
+      u <- paste('https://api.surveymonkey.net/v3/surveys/',survey,'/details?',sep='')  
+    }
+    else 
+      u <- paste('https://api.surveymonkey.net/v3/surveys/',survey,'?',sep='')  
+    if(!is.null(oauth_token)) {
+      token <- paste('bearer', oauth_token)
+    }
     else
         stop("Must specify 'oauth_token'")
     h <- add_headers(Authorization=token,
                      'Content-Type'='application/json')
-    b <- toJSON(list(survey_id = survey), auto_unbox = TRUE)
-    out <- POST(u, config = h, ..., body = b)
+    out <- GET(u, config = h, ...)
     stop_for_status(out)
-    content <- content(out, as='parsed')
-    if(content$status != 0) {
-        warning("An error occurred: ",content$errmsg)
-        return(content)
-    } else
-        structure(content$data, class='sm_survey')
+    content <- parsed_content(out)
+    # if(content$status != 0) {
+    #     warning("An error occurred: ",content$errmsg)
+    #     return(content)
+    # } else
+    structure(content, class = "sm_survey")
 }
 
 surveyquestions <- function(
     survey,
     details,
-    api_key = getOption('sm_api_key'),
     oauth_token = getOption('sm_oauth_token'),
     ...
 ){
     if (!missing(survey)) {
-        d <- surveydetails(survey, api_key = api_key, oauth_token = oauth_token, ...)
+        d <- surveydetails(survey, oauth_token = oauth_token, ...)
     } else {
         d <- details
     }
