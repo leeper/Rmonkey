e0ab98698ef0a37a23e8736a98a7b1f19d151c31
Sean Fahey <seanofahey@gmail.com>
2016-12-28 09:19:58 -0500

    created new documentation for smlogin

--- R/smlogin.r
+++ R/smlogin.r
@@ -1,3 +1,18 @@
+#' smlogin
+#' 
+#' obtains a long lasting oauth token for API access.
+#' 
+#' This function takes as input a SurveyMonkey API account client ID and
+#' secret code and conducts the oauth2 authentication process to return
+#' an oauth token.  The client_id and secret values can be obtained from the settings
+#' section of the appropriate SurveyMonkey apps page https://developer.surveymonkey.com/apps/
+#' 
+#' @param client_id Your SurveyMonkey App client_id. By default, retrieved from \code{getOption('sm_client_id')}.
+#' @param secret Your API secret key. By default, retrieved from \code{getOption('sm_secret')}.
+#' @param redirect_uri Default value is \samp{http://localhost:1410}. No other value is allowed. This must be the redirect URL registered for your application in your Survey Monkey developer account.
+#' @param response_type Default value is \dQuote{code}. No other values are allowed.
+#' @return oauth_token
+
 smlogin <- function(client_id = getOption('sm_client_id'),
                     secret = getOption('sm_secret'),
                     redirect_uri = 'http://localhost:1410',
