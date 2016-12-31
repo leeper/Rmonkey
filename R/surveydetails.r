f1a265a44a85afc2749c47b83b962ca4eed08e16
Sean Fahey <seanofahey@gmail.com>
2016-12-31 08:45:24 -0500

    added comment detailing the list structure form the JSON response

--- R/surveydetails.r
+++ R/surveydetails.r
@@ -1,3 +1,59 @@
+# surveydetails
+#
+# This function returns details about a SurveyMonkey survey in the list structure based on the
+# JSON response format
+#
+# $response_count : int = total number of responses
+# $pages_count : int = number of pages in the survey
+# $buttons_text : list of chr = display text for buttons
+# $custom_variables : named list = ?
+# $nickname : chr = short name for survey
+# $id : chr = SurveyMonkey id for the survey
+# $question_count : int = total number of questions on the survey
+# $category : chr = broad category for survey 
+# $preview : chr = web browsable url to preview the survey
+# $is_owner : logical = is user owner of the survey(?)
+# $language : chr = langauge the survey is written in
+# $date_modified : chr = date/time when survey was last modified
+# $title : chr = survey title
+# $analyze_url : chr = web browsable url to analyze responses
+# $pages : list
+#   $ href : chr = api accessible weblink for data on this page
+#   $ description : chr = text displayed at top of page
+#   $ questions : list = length is the number of questions on the page
+#     $ display_options : list
+#       $ show_display_number : logical 
+#     $ sorting : 
+#     $ family : chr = general style of question
+#     $ subtype : chr = detailed style of question
+#     $ required :
+#       $ text: chr = text to display if not answered(?)
+#       $ amount : chr = 
+#       $ type : chr = ?
+#     $ answers : list
+#       $ choices : list
+#         $ visible : logical 
+#         $ text : chr = answerchoice text
+#         $ position : int = answerchoice position
+#         $ id : chr = answerchoice id
+#     $ visible : logical 
+#     $ href : api accessible weblink for this question
+#     $ headings : list
+#     $ heading : chr = HTML text displayed with question
+#     $ position : int = display position for question on the page
+#     $ validation : 
+#     $ id : chr = id for the question
+#     $ forced_ranking : logical 
+#   $ title : chr
+#   $ position : int
+#   $ id : id for page
+#   $ question_count : int = number of questions on this page
+# $summary_url : chr = web browsable url for survey summary
+# $href : chr = api accessible web link for survey data
+# $date_created : chr = date survey originally created
+# $collect_url : chr = web browsable url to collect responses
+# $edit_url : chr = web browsable url to edit the survey
+
 surveydetails <- function(
     survey,
     question_details = FALSE,
