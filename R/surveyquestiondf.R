1807154eeb4433c7da68c93f41c3d3a159d7b3c5
Sean Fahey <seanofahey@gmail.com>
2017-01-01 18:49:51 -0500

    added comments and fixed row id and text function

--- R/surveyquestiondf.R
+++ R/surveyquestiondf.R
@@ -5,6 +5,25 @@ surveyquestiondf <- function(survey) {
   df <- data.frame()
   sd <- surveydetails(survey, question_details = TRUE)
   questions <- do.call('c', lapply(details$pages, function(i) i[['questions']]))
+
+  
+  
+  # answerrows <- do.call('c', lapply(questions, function(i) i[['answers']][['rows']]))
+  # answerchoices <- answerchoices <- do.call('c', lapply(questions, function(i) i[['answers']][['choices']]))
+  
+  # q_df <- do.call(rbind, lapply(questions, function(x) data.frame(question_id = x$id, question_type = x$family, question_subtype = x$subtype, question_text = x$heading, stringsAsFactors = FALSE)))
+  # ac_df <- do.call(rbind, lapply(answerchoices, function(x) data.frame(answerchoice_text = x$text, answerchoice_id = x$id, stringsAsFactors = FALSE)))
+  # ar_df <- do.call(rbind, lapply(answerrows, function(x) data.frame(subquestion_text = x$text, subquestion_id = x$id, stringsAsFactors = FALSE)))
+
+  # these work but don't preserve the question ID in the ar and ac frames preventing joining
+  
+  # experiment to extract row data and then apply question ids as names
+  # q_id <- do.call('c', lapply(questions, function(i) i[['id']]))
+  # ar <- lapply(questions, function(i) i[['answers']][['rows']])
+  # write the question_id into the answer row list prior to unpacking
+  # for (i in 1:length( ar) ) {if (!is.null(ar[[i]])) {ar[[i]]$rows$question_id <- questions[[i]]$id}}
+  # setNames(ar, q_id)
+  
   for (i in questions) {
     id <- i$id
     family <- i$family
@@ -12,10 +31,12 @@ surveyquestiondf <- function(survey) {
     heading <- i$heading
     if (!is.null(i$answer$rows)) {
       for (j in i$answer$rows) {
+        row_id <- j$id
         row_text <- j$text
         newrow <-
           data.frame(
             id,
+            row_id,
             family,
             subtype,
             heading,
@@ -26,10 +47,11 @@ surveyquestiondf <- function(survey) {
         df <- rbind(df, newrow)
       }
     } else {
-      row_text <- ''
-      newrow <-
-        data.frame(
+      row_id <- NA
+      row_text <- NA
+      newrow <- data.frame(
           id,
+          row_id,
           family,
           subtype,
           heading,
